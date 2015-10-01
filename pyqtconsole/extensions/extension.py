3f303f7125ee4a8cb3c9a5ced94c04539d806e7e
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2015-10-01 17:35:04 +0200

    Removed qt dependency Removed possibility to access extensions by name

--- pyqtconsole/extensions/extension.py
+++ pyqtconsole/extensions/extension.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
-from ..qt import QtCore
-
 class ExtensionManager(object):
     def __init__(self, owner):
         self._owner = owner
-        self._extension_list = []
+        self._extension_dict = {}
 
     def install(self, ext_cls):
         ext = ext_cls()
-        ext.install(self._owner)
-        self._extension_list.append(ext)
+        ext._ext_mngr = self
+        ext.install()
+        self._extension_dict[id(ext)] = ext
 
 
-class Extension(QtCore.QObject):
-    # Abstract
-    def install(self, parent):
+class Extension(object):
+    #Abstract
+    def install(self):
         pass
+
+    def owner(self):
+        return self._ext_mngr._owner
