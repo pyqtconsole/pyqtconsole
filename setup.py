0ae1bcb86c0e45c8d2cca32143397c34ee6c617c
Thomas Gläßle <t_glaessle@gmx.de>
2019-04-10 13:51:41 +0200

    Use setuptools to add some additional metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 import pyqtconsole
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='pyqtconsole',
+setup(
+    name='pyqtconsole',
     version=pyqtconsole.__version__,
     description=pyqtconsole.__description__,
     author=pyqtconsole.__author__,
     author_email=pyqtconsole.__author_email__,
     url=pyqtconsole.__url__,
+    license='MIT',
     long_description=open('README.rst').read(),
-    packages=['pyqtconsole', 'pyqtconsole.qt', 'pyqtconsole.extensions'])
+    long_description_content_type='text/x-rst',
+    keywords='interactive interpreter console shell autocompletion jedi qt',
+    packages=['pyqtconsole', 'pyqtconsole.qt', 'pyqtconsole.extensions'],
+    python_requires='>=2.7',
+    extras_require={
+        'autocomplete': ['jedi'],
+    },
+    zip_safe=True,
+    include_package_data=True,
+)
