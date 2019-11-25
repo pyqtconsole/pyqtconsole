927c889f7e0c41e2fc6b99d5d7fa85af172e107c
Thomas Gläßle <t_glaessle@gmx.de>
2019-11-25 17:50:06 +0100

    Fix overlong line style issue

--- pyqtconsole/__init__.py
+++ pyqtconsole/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __version__ = '1.1.4'
-__description__ = 'Light weight python interpreter, easy to embed into Qt applications'
+__description__ = \
+    'Lightweight python console, easy to embed into Qt applications'
 __author__ = 'Marcus Oskarsson'
 __author_email__ = 'marcus.oscarsson@esrf.fr'
 __url__ = 'https://github.com/marcus-oscarsson/pyqtconsole'
