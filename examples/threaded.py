33830111c9d5dbad679c9e6edbbef64fb32bd184
Jacob Williams <jacobwilliams@users.noreply.github.com>
2026-02-17 01:32:28 -0600

    added a function to clear the console (#89)
    * added a function to clear the console
    
    Fixes #58
    
    * added clear function to example
    
    * fix example
    
    * added a function to clear the console
    
    Fixes #58
    
    * added clear function to example
    
    * fix example
    
    * Updated changelog + added features section in the readme
    
    ---------
    
    Co-authored-by: Robert <robert.soor@gmail.com>

--- examples/threaded.py
+++ examples/threaded.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
 
     console = PythonConsole(shell_cmd_prefix=True)
     console.push_local_ns('greet', greet)
+    console.interpreter.locals["clear"] = console.clear
     console.show()
     console.eval_in_thread()
     sys.exit(app.exec_())
